09eb5c2013890d486efe2bc790b4fd933f56f359
rustydcoder <rustydcoder@gmail.com>
2020-10-25 12:56:24 +0100

    Updates

---
+++ precache-manifest.7ef8ea3c97ddd39a57d3c89e17f953b4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5505fbd616fa2fca9a0eaafde841cd9",
+    "url": "/GOT-fanapp/index.html"
+  },
+  {
+    "revision": "0e434ef3749d98c4afc9",
+    "url": "/GOT-fanapp/static/css/2.616f65ab.chunk.css"
+  },
+  {
+    "revision": "d1348906e61b3a287e66",
+    "url": "/GOT-fanapp/static/css/main.e84ff4c6.chunk.css"
+  },
+  {
+    "revision": "0e434ef3749d98c4afc9",
+    "url": "/GOT-fanapp/static/js/2.32881062.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/GOT-fanapp/static/js/2.32881062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1348906e61b3a287e66",
+    "url": "/GOT-fanapp/static/js/main.627cab81.chunk.js"
+  },
+  {
+    "revision": "ab145f766626b85645b5",
+    "url": "/GOT-fanapp/static/js/runtime-main.a851b28a.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/GOT-fanapp/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/GOT-fanapp/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/GOT-fanapp/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/GOT-fanapp/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/GOT-fanapp/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/GOT-fanapp/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/GOT-fanapp/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/GOT-fanapp/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/GOT-fanapp/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/GOT-fanapp/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/GOT-fanapp/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/GOT-fanapp/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/GOT-fanapp/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/GOT-fanapp/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/GOT-fanapp/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "2d81a83e89568f739d09a0d418bf6cbb",
+    "url": "/GOT-fanapp/static/media/got-font.2d81a83e.png"
+  },
+  {
+    "revision": "9911204165c011e397d35a7a3dd3d78e",
+    "url": "/GOT-fanapp/static/media/hero.99112041.jpg"
+  },
+  {
+    "revision": "3ba2c67bbe0280ebc7da670c332925c9",
+    "url": "/GOT-fanapp/static/media/logo-brand.3ba2c67b.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/GOT-fanapp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/GOT-fanapp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/GOT-fanapp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/GOT-fanapp/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GOT-fanapp/precache-manifest.7ef8ea3c97ddd39a57d3c89e17f953b4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GOT-fanapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
